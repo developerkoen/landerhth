df8ee78b4bef7585624874e56d49aa3f4e85fd39
Koen <koenspam34@gmail.com>
2025-02-25 15:56:05 +0100

    Update html.js

--- html.js
+++ html.js
@@ -1,9 +1,7 @@
-// Zorg ervoor dat de juiste pagina wordt geladen zonder .html extensie
 window.onload = function() {
-    // Controleer de huidige URL van de gebruiker
-    if (window.location.pathname === "/") {
-        window.history.pushState({}, "", "/lander");
-    } else if (window.location.pathname === "/dashboard/login") {
-        window.history.pushState({}, "", "/dashboard/login");
+    // Zorg ervoor dat de juiste pagina wordt geladen zonder .html extensie
+    if (window.location.pathname === "/lander.html") {
+        // Verander de URL naar /lander zonder .html
+        window.history.pushState({}, "", "/");
     }
 };
