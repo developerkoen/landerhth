9a8e6d779c0e492d98317fdf6eb7ce838d5e2dca
Koen <koenspam34@gmail.com>
2025-02-25 15:57:47 +0100

    Delete html.js

--- html.js
+++
@@ -1,7 +0,0 @@
-window.onload = function() {
-    // Zorg ervoor dat de juiste pagina wordt geladen zonder .html extensie
-    if (window.location.pathname === "/lander.html") {
-        // Verander de URL naar /lander zonder .html
-        window.history.pushState({}, "", "/");
-    }
-};
